1f87821efb2016fda4fc64aba16158ae0b9b8609
farouky <pharouky@gmail.com>
2022-08-06 17:45:29 +0100

    Include footer.php !!!

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+        <div>
+            <a href="#"><img src="./assets/images/facebook.png" alt="facebook" /></a>
+            <a href="#"><img src="./assets/images/twitter.png" alt="twitter" /></a>
+            <a href="#"><img src="./assets/images/linkedin.png" alt="linkedin" /></a>
+            <a href="#"><img src="./assets/images/github.png" alt="github" /></a>
+        </div>
+        <div>
+            <p>© All Right Reserved 2022</p>
+        </div>
+    </footer>
+    <script src="./js/script.js"></script>
--- login.php
+++ login.php
@@ -75,18 +75,8 @@ if($db){
             </div>
         </div>
     </main>
-    <footer>
-        <div>
-            <a href="#"><img src="./assets/images/facebook.png" alt="facebook" /></a>
-            <a href="#"><img src="./assets/images/twitter.png" alt="twitter" /></a>
-            <a href="#"><img src="./assets/images/linkedin.png" alt="linkedin" /></a>
-            <a href="#"><img src="./assets/images/github.png" alt="github" /></a>
-        </div>
-        <div>
-            <p>© All Right Reserved 2022</p>
-        </div>
-    </footer>
-    <script src="./js/script.js"></script>
+
+    <?php include 'footer.php'; ?>
 </body>
 
 </html>
